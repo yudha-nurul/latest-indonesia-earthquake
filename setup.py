add6e6a9274a23e7a8670ff4557130774f2de24e
yudha.alfian <yudhanurulalfian@gmail.com>
2023-02-21 11:56:07 +0700

    format dasar setup.py

--- setup.py
+++ setup.py
@@ -8,24 +8,26 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE",
-    version="0.0.1",
-    author="Example Author",
-    author_email="author.@example.com",
-    description="A small example package",
+    name="latest_indonesia_earthquake",
+    version="0.1",
+    author="Yudha N. Alfian",
+    author_email="yudhanurulalfian@gmail.com",
+    description="this package will get the latest earthquake from BMKG",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/yudha-nurul/latest-indonesia-earthquake",
     project_url={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issue",
+        "Website": "https://yudhanurulalfian.wordpress.com",
     },
     classifiers=[
         "Programming Language ::Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable"
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    # package_dir={"": "src"},
+    # packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(),
     python_requires=">=3.6",
 )
 
